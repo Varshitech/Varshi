154f772c5dc19dea3ba9810532c80c4778e38ac4
Varshitech <116360580+Varshitech@users.noreply.github.com>
2022-11-10 18:23:46 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,12 @@
+class factorial
+{
+public static void main(String[]args)
+{
+int i, fact=2;
+int number=10;
+for(i=1; i<number; i++){
+fact=fact*i;
+}
+System.out.println("Factorial of five:"+fact);
+}
+}
