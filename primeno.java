de240c71e81f1c4e83f1ae7233c17f6579484e81
Varshitech <116360580+Varshitech@users.noreply.github.com>
2022-11-10 18:32:04 +0530

    Add files via upload

---
+++ primeno.java
@@ -0,0 +1,14 @@
+public class primeno
+{
+public static void main(String[]args)
+{
+int num=50;
+if(num%5==0)
+{
+System.out.println("Enter no is even");
+}
+else{
+System.out.println("Enter no is odd");
+}
+}
+}
