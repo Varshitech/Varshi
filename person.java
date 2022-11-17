95cc4b7e84bc84d4f7d2f6556e6d3a6706545a67
Varshitech <116360580+Varshitech@users.noreply.github.com>
2022-11-17 12:12:39 +0530

    Add files via upload

---
+++ person.java
@@ -0,0 +1,13 @@
+record person(String Name, int Age, String Gender, double ID){
+  person(){
+this("pandu", 21, "Female", 2001);}
+     public static void main(String[] args){
+person p = new person("pandu", 21, "Female", 2001);
+person p1= new person("sunny", 18, "Male", 2004);
+person p2= new person("janu", 4, "Female", 2018);
+  System.out.println(p);
+  System.out.println(p1);
+  System.out.println(p2);
+}
+}
+
